56f2923b148469a81eaa0dd804dd0b1cfe79a0e4
Lauri <lauri.nomtak@mail.ee>
2015-09-22 10:01:37 +0300

    Navigeerimatu link [Delivers #103878550]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -1,15 +1,16 @@
-Ôªø<!doctype html>
+?<!doctype html>
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>Lauri N√µmtak</title>
+    <title>Lauri Nımtak</title>
 </head>
 <body>
 
 <button onclick="nupp()">Tere maailm!</button>
 
 <a href="www.khk.ee" onClick="link()">Tere maailm!</a>
-<a href="" onclick="jaamesiia()">J√§√§me siia</a>
+
+<a href="" onclick="jaamesiia()">J‰‰me siia</a>
 
 <script>
     function nupp() {
@@ -25,7 +26,7 @@
 
 <script>
     function jaamesiia() {
-        alert("J√§√§me siia")
+        alert("J‰‰me siia")
     }
 </script>
 
