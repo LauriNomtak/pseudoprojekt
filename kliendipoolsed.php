2a0151c90f7486ce2c754410da7275d52c1876e0
Lauri <lauri.nomtak@mail.ee>
2015-09-23 10:39:24 +0300

    7. jQuery taustavärvi muutmine [Delivers #103882350]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -1,11 +1,11 @@
 <!doctype html>
-<html lang="ee">
+<html lang="ee" xmlns="http://www.w3.org/1999/html">
 <head>
     <meta content="text/html; charset=utf-8" http-equiv="content-type">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <title>Lauri Nõmtak</title>
 </head>
-<body>
+<body id=body style="background-color:white">
 
 <button onclick="nupp()">Tere maailm!</button>
 
@@ -13,8 +13,12 @@
 
 <a href="" onclick="jaamesiia()">Jääme siia</a>
 
-<img id="kass" onclick="kasskoeraks()" src="http://g3.nh.ee/images/pix/900x585/7QYBvGhCuSg/kass-66103366.jpg"
+<img id="kass" src="http://g3.nh.ee/images/pix/900x585/7QYBvGhCuSg/kass-66103366.jpg"
     style="width: 200px">
+
+<button id=red>Red</button>
+<button id=green>Green</button>
+<button id=blue >Blue</button>
 
 <script>
     function nupp() {
@@ -29,9 +33,22 @@
         alert("Jääme siia")
     }
 
-    function kasskoeraks() {
-        $("#kass").attr("src", "http://www.lahe.mobi/gallery/139/744046b015.jpg")
-        }
+    $('#kass').click(function() {
+        $('#kass').attr("src", "http://www.lahe.mobi/gallery/139/744046b015.jpg")
+    });
+
+
+    $('#red').click(function() {
+        $("body").css("background-color","red");
+    });
+
+    $('#green').click(function() {
+        $("body").css("background-color","green");
+    });
+
+    $('#blue').click(function() {
+        $("body").css("background-color","blue");
+    });
 </script>
 
 </body>
