c81fc5ceb543c6bda82230ae9b64f14d0a7db65f
Lauri <lauri.nomtak@mail.ee>
2015-10-12 15:34:54 +0300

    mysqli_connect(): (HY000/2005): Unknown MySQL server host [Delivers #105457924]

--- config.sample.php
+++ config.sample.php
@@ -1,7 +1,7 @@
 <?php
 
-define("DATABASE_HOSTNAME","");
-define("DATABASE_USERNAME", "");
-define("DATABASE_PASSWORD","");
-define("DATABASE_DATABASE","");
+define("DATABASE_HOSTNAME", " ");
+define("DATABASE_USERNAME", " ");
+define("DATABASE_PASSWORD", " ");
+define("DATABASE_DATABASE", " ");
 
