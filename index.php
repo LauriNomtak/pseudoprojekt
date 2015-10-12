3f3331fc801e4cb3e73a38fa22a64dbc77a035f5
Lauri <lauri.nomtak@mail.ee>
2015-10-12 15:26:28 +0300

    mysqli_connect(): (HY000/2005): Unknown MySQL server host [Delivers #105457924]

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 //
-require "config.sample.php";
+require "sample.php";
 
 $db = mysqli_connect(DATABASE_HOSTNAME, DATABASE_USERNAME , DATABASE_PASSWORD, DATABASE_DATABASE);
 mysqli_query($db, "SET NAMES 'utf8'");
